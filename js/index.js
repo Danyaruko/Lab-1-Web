acc9ea9e872158651570de241dc545319a66fe6f
Danyaruko <danilpatana1905@gmail.com>
2021-12-02 00:21:58 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,6 +8,5 @@ const body = document.getElementById("body");
  function toggleMenu() {
     navLinks.classList.toggle(OPEN_CLASSNAME);
     headerBurger.classList.toggle(OPEN_CLASSNAME);
-    body.classList.toggle("locke");
 }
 
